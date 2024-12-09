d42838ea96d5c56f455b76c57fffbc20691533ae
Abhishek <Abhishekgwari14sep@gmail.com>
2024-12-09 17:07:36 +0530

    Add project files.

---
+++ ValidationAttribute/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ValidationAttribute.Models;
+
+namespace ValidationAttribute.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Student std)
+        {
+
+            return View();
+
+            // Model state
+            //if (ModelState.IsValid) { 
+            //return "Name is" + std.Name;
+            //}
+            //else
+            //{
+            //    return "Validation FAILED";
+            //}
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ValidationAttribute/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidationAttribute.Models
+{
+    public class Student
+
+    {
+        [Required(ErrorMessage ="Name is  must")]
+        [StringLength(15,MinimumLength =3, ErrorMessage ="Name must be  within 3 to 15 character ")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Email is  must")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Age is  must")]
+        [Range(18,60,ErrorMessage ="Age must be within 18 to 60")]
+        public int? Age { get; set; }                           // ? its null
+    }
+}
